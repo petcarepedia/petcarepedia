582a69f25c0b0162e04aa7535a3bc5c28e2ac160
heynokimus <heymush00@gmail.com>
2023-07-19 14:54:03 +0900

    🎉Start Project

--- src/main/java/com/project/petcarepedia/controller/BoardController.java
+++ src/main/java/com/project/petcarepedia/controller/BoardController.java
@@ -11,7 +11,7 @@
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.PostMapping;
-
+//
 //@Controller
 //public class BoardController {
 //    @Autowired
